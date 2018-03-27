8d4a5a8c78bcf1e38aea4ed90644915d3ce0139b
Unknown <Jassi11@gmx.de>
2018-03-27 21:29:03 +0200

    Fixed accidental fullscreen/added  error handler

--- conf.lua
+++ conf.lua
@@ -5,7 +5,7 @@ function love.conf(t)
   t.window.display = 2                -- Index of the monitor to show the window in (number)
   t.window.width = 1024
   t.window.height = 768
-  t.window.fullscreen = true         -- Enable fullscreen (boolean)
+  t.window.fullscreen = false         -- Enable fullscreen (boolean)
 
   t.window.fullscreentype = "exclusive" -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
   t.window.borderless = false 
