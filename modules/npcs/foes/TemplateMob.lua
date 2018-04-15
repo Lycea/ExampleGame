97ea1c280f2517ea69a8c3f9033948d2fd888057
Lycea <Jassi11@gmx.de>
2018-04-15 22:00:24 +0200

    -> added a template class
    -> simple template class  close # 12

---
+++ modules/npcs/foes/TemplateMob.lua
@@ -0,0 +1,51 @@
+local tmp = {}
+
+local function moveTo()
+    
+end
+
+local function attack()
+    
+end
+
+local function follow()
+    
+end
+
+local behaviors =
+{
+-- moveTo,Attack
+-- moveTo
+}
+
+
+function tmp:new (o)
+      o = o or {}   -- create object if user does not provide one
+      o.name = "TemplateMob"
+      
+      o.health   = 5
+      o.strength = 1
+      o.defense  = 1
+      o.speed    = 1 --tile per move
+      o.state    = "sleep"  -- following,active,sleep,fighting,roaming,inactive
+      o.effects  ={}  --table for fight effects ..
+      
+      o.min_spawn
+      o.max_spawn
+      
+      o.pos  = o.pos or 0
+      setmetatable(o, self)
+      self.__index = self
+      return o
+end
+
+function tmp:update(dt)
+    
+end
+
+
+function tmp:draw()
+    
+end
+
+    
